56eaa03adbcff33c30f92ced3116a192c72bde3a
pirahawk <piran.tata@gmail.com>
2020-01-18 14:24:43 +0000

    Initial server solution setup

---
+++ AgentPortal/AgentPortal/Controllers/HealthCheckController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AgentPortal.Controllers
+{
+    [Route("health")]
+    public class HealthCheckController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult HealthCheck()
+        {
+            return Ok("Agent Portal running");
+        }
+    }
+}
