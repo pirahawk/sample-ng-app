aa8058e9d4ad876e137cb548d2861346f7ff0dca
pirahawk <piran.tata@gmail.com>
2020-01-21 18:47:03 +0000

    fixed npm build issue

--- AgentPortal/AgentPortal/Pages/Index.cshtml.cs
+++ AgentPortal/AgentPortal/Pages/Index.cshtml.cs
@@ -1,8 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.IO;
 using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
 
@@ -11,15 +9,25 @@ namespace WebApplication4.Pages
     public class IndexModel : PageModel
     {
         private readonly ILogger<IndexModel> _logger;
+        private readonly IWebHostEnvironment _env;
 
-        public IndexModel(ILogger<IndexModel> logger)
+        public IndexModel(ILogger<IndexModel> logger, IWebHostEnvironment env)
         {
             _logger = logger;
+            _env = env;
         }
 
-        public void OnGet()
+        public bool ShouldIncludeEs2015Modules()
         {
+            var contents = _env.WebRootFileProvider.GetDirectoryContents("js/portalClient");
 
+            if (!contents.Exists)
+            {
+                return false;
+            }
+
+            var hasEs2015Modules = contents.Any(asset => Path.GetFileName(asset.PhysicalPath).Contains("es2015"));
+            return hasEs2015Modules;
         }
     }
 }
